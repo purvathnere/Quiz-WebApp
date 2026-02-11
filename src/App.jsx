e09c4d6dca770b313ca0fb3aeb22597e8f820e0a
snehad95 <snehadhotre9395@gmail.com>
2026-02-11 16:07:24 +0530

    Added new folder and updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,55 @@
-
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Slideshow from "./components/Slideshow";
 import FeatureBoxes from "./components/FeatureBoxes";
 import Footer from "./components/Footer";
 import Info from "./components/Info";
+import CreateAccount from "./components/CreateAccount";
+import Login from "./components/Login";
+import Course from "./components/Course";
+import Exams from "./components/Exams";
+import About from "./components/About";
+import ExamCategories from "./ExamCategories";
+import HomePage from "./HomePage";
 
 
 
 function App() {
   return (
     <>
+      {/* Common Header */}
       <Header />
-      <Slideshow/>
-      <FeatureBoxes/>
-      <Info />
-    
 
+      {/* Routes */}
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Slideshow />
+              <FeatureBoxes />
+              <Info />
+              <ExamCategories/>
+              <HomePage/>
+            </>
+          }
+        />
 
+        {/* Create Account Page */}
+        
+        <Route path="/create-account" element={<CreateAccount />} />
+         <Route path="/Login" element={<Login />} />
+         <Route path="/Course" element={<Course />} />
+         <Route path="/Exams" element={<Exams />} />
+         <Route path="/About" element={<About />} />
+         <Route path="/FeatureBoxes" element={<FeatureBoxes />} />
+        </Routes>
 
-      
-      <Footer/>
+      {/* Common Footer */}
+      <Footer />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
